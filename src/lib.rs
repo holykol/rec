79b2e4af9c96807329f5422953c1a193cdf0d633
Timofey <ouxya@pm.me>
2020-11-24 23:28:05 +0300

    Basic parser

--- src/lib.rs
+++ src/lib.rs
@@ -5,18 +5,24 @@
 #![allow(unused_variables)]
 
 mod lex;
-use crate::lex::*;
+use crate::lex::{lex, Token};
 
 use std::collections::{HashMap, HashSet};
 use std::error::Error;
 use std::fs;
 use std::path::Path;
 
+use lazy_static::lazy_static;
 use regex::Regex;
+
+lazy_static! {
+    static ref FIELD_RX: Regex = Regex::new("[a-zA-Z%][a-zA-Z0-9_]*").unwrap();
+    static ref ENUM_RX: Regex = Regex::new("[a-zA-Z0-9][a-zA-Z0-9_-]*").unwrap();
+}
 
 type Key = String;
 
-#[derive(Default)]
+#[derive(Default, Debug)]
 pub struct DB {
     rectype: Option<String>,
     primary_key: Option<String>,
@@ -27,8 +33,8 @@ pub struct DB {
 
 pub type Record = HashMap<Key, Field>;
 
+#[derive(Debug)]
 pub struct Field {
-    name: Key,
     value: String,
     kind: FieldKind,
 }
@@ -42,8 +48,8 @@ pub struct Meta {
 
 #[derive(Debug)]
 enum Constraint {
-    Allowed,
     Mandatory,
+    Allowed,
     Phohibit,
 }
 
@@ -57,7 +63,7 @@ pub enum FieldKind {
     Email,
     Field,
     UUID,
-    Regexp(Box<Regex>),
+    Regexp(Regex),
     Viz(String),
     Enum(HashSet<String>),
 }
@@ -76,12 +82,13 @@ impl DB {
     }
 
     fn parse(&mut self, tokens: Vec<Token>) -> Result<(), Box<dyn Error>> {
-        use crate::lex::Token::*;
+        let mut records = Vec::new();
+        let mut current_record: Option<Record> = None;
 
         for token in tokens.iter() {
             println!("token: {:?}", token);
             match token {
-                Keyword(keyword, value) => match keyword.as_ref() {
+                Token::Keyword(keyword, value) => match keyword.as_ref() {
                     "rec" => self.rectype = Some(value.clone()),
                     "type" => {
                         let args: Vec<&str> = value.split_whitespace().collect();
@@ -97,40 +104,99 @@ impl DB {
                     }
                     _ => (),
                 },
-                Blank => unimplemented!(),
-                _ => println!("unkown token {:?}", token),
+                Token::Field(key, value) => {
+                    if current_record.is_none() {
+                        current_record = Some(Default::default())
+                    }
+
+                    match current_record {
+                        Some(ref mut rec) => {
+                            let f = Field {
+                                value: value.clone(),
+                                kind: FieldKind::Line,
+                            };
+
+                            rec.insert(key.clone(), f);
+                        }
+                        None => unreachable!(),
+                    }
+                }
+
+                Token::Blank => {
+                    if let Some(rec) = current_record.take() {
+                        records.push(rec);
+                    }
+                }
             }
         }
+
+        if let Some(rec) = current_record {
+            records.push(rec);
+        }
+
+        self.records = records;
 
         Ok(())
     }
 }
 
 fn parse_type(args: Vec<&str>) -> Result<FieldKind, Box<dyn Error>> {
-    let &tt = args.get(1).unwrap();
+    use FieldKind::*;
+
+    let &tt = args.get(1).ok_or("expected field name")?;
+
+    if !FIELD_RX.is_match(&tt) {
+        return Err(format!("invalid field name: {}", tt).into());
+    }
 
     match tt {
-        "line" => Ok(FieldKind::Line),
-        "int" => Ok(FieldKind::Int),
-        "real" => Ok(FieldKind::Real),
-        "bool" => Ok(FieldKind::Bool),
-        "date" => Ok(FieldKind::Date),
-        "email" => Ok(FieldKind::Date),
-        "field" => Ok(FieldKind::Field),
+        "line" => Ok(Line),
+        "int" => Ok(Int),
+        "real" => Ok(Real),
+        "bool" => Ok(Bool),
+        "date" => Ok(Date),
+        "email" => Ok(Date),
+        "field" => Ok(Field),
+        "uuid" => Ok(UUID),
         "regexp" => {
-            let rx = args.get(2).unwrap();
+            let rx = args
+                .get(2)
+                .ok_or("expected regexp definition as third field")?
+                .strip_prefix("/")
+                .ok_or("expected regexp to begin with slash")?
+                .strip_suffix("/")
+                .ok_or("expected regexp to end with slash")?;
+
             let compiled = Regex::new(rx)?;
-            Ok(FieldKind::Regexp(box compiled))
+            Ok(Regexp(compiled))
         }
-        "uuid" => Ok(FieldKind::UUID),
         "viz" => {
-            // TODO validate field
-            unimplemented!()
+            let key = args
+                .get(2)
+                .ok_or("expected field reference as second field")?;
+
+            match FIELD_RX.is_match(key) {
+                true => Ok(FieldKind::Viz(key.to_string())),
+                false => Err(format!("invalid viz value: {}", key).into()),
+            }
         }
         "enum" => {
-            unimplemented!()
+            if args.len() < 3 {
+                return Err("expected at least one enum variant".into());
+            }
+
+            let mut variants = HashSet::with_capacity(args.len() - 2);
+
+            for v in args.iter().skip(2).map(|s| s.to_string()) {
+                match ENUM_RX.is_match(&v) {
+                    true => variants.insert(v),
+                    false => return Err(format!("invalid enum value: {}", v).into()),
+                };
+            }
+
+            Ok(Enum(variants))
         }
-        _ => Err("unknown type value".into()),
+        _ => Err(format!("unknown type: {}", tt).into()),
     }
 }
 #[cfg(test)]
@@ -139,8 +205,9 @@ mod tests {
     #[test]
     #[ignore = "come back in around 20 years"]
     fn it_works() {
-        //let path = Path::new("/home/t/dev/rust/rec/src/test.rec");
-        //let _db = DB::new(&path).unwrap();
+        let buf = include_str!("/home/t/dev/rust/rec/src/test.rec");
+        let db = DB::new(buf.to_string()).unwrap();
+        println!("{:?}", db);
     }
 
     #[test]
@@ -150,12 +217,44 @@ mod tests {
     }
 
     #[test]
-    fn parser_field_type() {
+    fn parser_type_line() {
         let db = DB::new("%type: Book line".to_owned()).unwrap();
         let meta = db.types.get(&"Book".to_owned()).unwrap();
-        match meta.kind.as_ref().unwrap() {
-            FieldKind::Line => (),
-            _ => panic!("Field type should be line"),
+        assert!(matches!(meta.kind, Some(FieldKind::Line)))
+    }
+
+    #[test]
+    fn parser_type_regex() {
+        let db = DB::new("%type: Phone regexp /^[0-9]{10}$/".to_owned()).unwrap();
+        let meta = db.types.get(&"Phone".to_owned()).unwrap();
+
+        if let Some(FieldKind::Regexp(ref rx)) = meta.kind {
+            assert!(rx.is_match("0123456789"));
+            assert!(!rx.is_match("blah"));
+            return;
         }
+        panic!("unexpected type")
+    }
+
+    #[test]
+    fn parser_type_enum() {
+        let db = DB::new("%type: Status enum Loading Done Error".to_owned()).unwrap();
+        let meta = db.types.get(&"Status".to_owned()).unwrap();
+
+        if let Some(FieldKind::Enum(ref variants)) = meta.kind {
+            assert_eq!(variants.len(), 3);
+            assert!(variants.contains("Loading"));
+            assert!(variants.contains("Done"));
+            assert!(variants.contains("Error"));
+            return;
+        }
+        panic!("unexpected type")
+    }
+
+    #[test]
+    fn parser_records() {
+        let db = DB::new("hello: world\nblah: blah\n\nhello: mom\nblah: bruh".to_owned()).unwrap();
+
+        assert_eq!(db.records.len(), 2);
     }
 }
